2a1000addf8ed2ffa4d50f99c889e156fee7a791
Abraham Álvarez Gómez <abrahamalvarezgomez2507@gmail.com>
2023-08-15 13:40:15 +0100

    feat:(./src/Main/main.tsx) refactor main.tsx deleting spaces and comments

--- src/components/Main/main.tsx
+++ src/components/Main/main.tsx
@@ -1,9 +1,17 @@
 import LIBRARY from "../../constants/constats";
 import main from "./Main.module.css";
 import Book from "../Book/book";
-// import bookItem from "./../Book/BookItem.module.css";
-// import Header from "../Header/header";
 
+/**
+ * The `body` function returns a JSX element representing the body of a web page,
+ * including a books section.
+ * @returns The body component is being returned. It is a JSX element that
+ * represents the body of a web page. It includes a className attribute that is set
+ * to the value of the main.settings variable. Inside the body component, there is
+ * a div element with a className attribute set to the value of the
+ * main.booksSection variable. The content of this div is the result of calling the
+ * displayBooksList() function.
+ */
 const body = () => {
   return (
     <body className={main.settings}>
@@ -13,6 +21,13 @@ const body = () => {
   );
 };
 
+/**
+ * The function `displayBooksList` maps over an array of books in the `LIBRARY`
+ * variable and returns a list of book cards.
+ * @returns The `displayBooksList` function is returning an array of JSX elements.
+ * Each element represents a book card and contains a `Book` component with the
+ * book data from the `LIBRARY` array.
+ */
 const displayBooksList = () => {
   return LIBRARY.map((library) => {
     return (
