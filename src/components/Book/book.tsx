cd65573ce361a4919dca1ff2e9e02d3589d36167
Abraham Álvarez Gómez <abrahamalvarezgomez2507@gmail.com>
2023-08-15 13:03:17 +0100

    feat:(./src/Book) Add NextUI Card width coustom properties

--- src/components/Book/book.tsx
+++ src/components/Book/book.tsx
@@ -1,23 +1,61 @@
-import bookItem from './BookItem.module.css';
-import Books from '../../interfaces/books';
-const Book = (props:Books) => {
-    return(
-       
-            <section className={bookItem.booksCards}>
-                <div>
-                    <img src={props.cover} />
-                </div>
-                
-                    <ol>
-                        <li>{props.pages}</li>
-                        <li>{props.genre}</li> 
-                        <li>{props.year}</li> 
-                        <li>{props.ISBN}</li>
-                    </ol>
-                
-            </section>
-           
-    );
+// import bookItem from '../Book/BookItem.module.css';
+import { ReactElement } from "react";
+import Books from "../../interfaces/books";
+import { Card, Skeleton } from "@nextui-org/react";
+const Book = (props: Books) => {
+  return customSkeletonCardNextUI();
+
+  // <section>
+  //     <div>
+  //         <img src={props.cover} />
+  //     </div>
+
+  //         <ol>
+  //             <li>{props.pages}</li>
+  //             <li>{props.genre}</li>
+  //             <li>{props.year}</li>
+  //             <li>{props.ISBN}</li>
+  //         </ol>
+
+  // </section>
 };
 
+const customSkeletonCardNextUI = (): ReactElement => {
+  return (
+    <Card className="w-[200px] h-[250px] space-y-5 p-4" radius="lg">
+      {skeletonImgSection()}
+      {skeletonPropsSetion()}
+    </Card>
+  );
+};
+
+const skeletonImgSection = (): ReactElement => {
+  return (
+    <Skeleton className="rounded-lg">
+      <div className="h-24 rounded-lg bg-default-300"></div>
+    </Skeleton>
+  );
+};
+
+const skeletonPropsSetion = (): ReactElement => {
+  return (
+    <div className="space-y-3">
+      {skeletonTextSM("w-3/5", "rounded-lg")}
+      <Skeleton className="w-4/5 rounded-lg">
+        <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
+      </Skeleton>
+      <Skeleton className="w-2/5 rounded-lg">
+        <div className="h-3 w-2/5 rounded-lg bg-default-300"></div>
+      </Skeleton>
+    </div>
+  );
+};
+
+const skeletonTextSM = (width: string, rounded: string): ReactElement => {
+  return (
+    <Skeleton className={`${width}` + ` ` + `${rounded}`}>
+      <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
+    </Skeleton>
+  );
+};
 export default Book;
--- src/components/Main/main.tsx
+++ src/components/Main/main.tsx
@@ -1,42 +1,26 @@
+import LIBRARY from "../../constants/constats";
+import main from "./Main.module.css";
+import Book from "../Book/book";
+// import bookItem from "./../Book/BookItem.module.css";
+// import Header from "../Header/header";
 
-import { ReactElement } from 'react';
-import LIBRARY from '../../constants/constats';
-import main from './Main.module.css';
-import Book from '../Book/book';
-import bookItem from './../Book/BookItem.module.css';
-import Header from '../Header/header';
+const body = () => {
+  return (
+    <body className={main.settings}>
+      {/* <Header name="Nathan" age={27} occupation="Software Developer" /> */}
+      <div className={main.booksSection}>{displayBooksList()}</div>
+    </body>
+  );
+};
 
-
-const body = ():ReactElement => {
-  
-   return(
-      <body className={main.settings}>
-
-         {/* <Header name="Nathan" age={27} occupation="Software Developer" />
-         <div className={main.booksGrid}>
-         <section className={bookItem.booksCardsList}>
-            {{displayBooksList()}; }
-     
-         </section>
-           
-         </div> */}
-
-       
-        
-      </body>
-      
-      
-   );
-}; 
-
-// const displayBooksList = () =>{
-//    return(
-//       LIBRARY.map( (library) => {
-//          return (
-//              <Book {...library.book} />
-//          )
-//        })
-//    );
-// }
+const displayBooksList = () => {
+  return LIBRARY.map((library) => {
+    return (
+      <div className={main.booksCards}>
+        <Book {...library.book} />
+      </div>
+    );
+  });
+};
 
 export default body;
